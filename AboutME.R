1becf4a35e851cdcff80b30c3fc524dc83994bf6
Anwyn Springfield <aes223@lehigh.edu>
2019-10-30 16:28:26 -0400

    Resume
    A written description about my professional history, my volunteer work, and my education.

---
+++ AboutME.R
@@ -0,0 +1,11 @@
+#Anwyn E. Springfield
+#
+#PROFESSIONAL HISTORY
+#In the past, I have been a babysitter and a marketing intern at a local business called Janna Ugone and Company. Now, as a full time student, I work in the athletic department at Lehigh University as a cross country and track team work study student.
+#
+#VOLUNTEER WORK
+#I have spent a large amount of time in the past year volunteering at Habitat for Humanity. I have interned for the organization, participated in the group builds, and volunteered at the Habitat ReStore in the Lehigh Valley.
+#
+#EDUCATION
+#I graduated from Northampton High School in Massachusetts last May and now I am currently a first year at Lehigh University.
+
